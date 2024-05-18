3c570a4718932271bee9a6ffe04d44998500d806
Zaid Abdulameer <zabdulam33r@gmail.com>
2024-05-18 01:18:50 -0400

    Nicer home page

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,17 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-        <div className="flex flex-col items-center justify-between p-8 bg-[url('/tech-bg.jpg')]">
-          <div className="bg-black/75 width-full h-full"></div>
+      <div className="h-screen bg-no-repeat bg-[url('/tech-bg.jpg')]">
+        <div className="flex h-screen width-screen flex-col items-center p-8 bg-black/60">
           <Image src="/logo.png" width={400} height={400}></Image>
-          <p className="text-white">Ace your next interview</p>
+          <p className="text-white text-xl font-bold">Ace your next interview with the help of AI</p>
+          <ul className="text-white mt-32 text-center space-y-5 text-xl">
+            <li> Practice answering interview questions generated using Open AI based on your desired job </li>
+            <li> Get personalized interview tips and a score </li>
+            <li> Login and track your progress to take your interviewing skills to the next level</li>
+          </ul>
+          <Button className="mt-32 text-xl pl-12 pr-12 bg-teal-500 block mx-auto align-text-top">Get started</Button>
+        </div>
       </div>
-      <div className="width-full h-screen flex items-center justify-center pb-24 bg-white">
-        <Button>Get started</Button>
-      </div>
-    </div>
   );
 }
